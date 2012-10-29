33098caaa8425755c7a6060f7b3909b2d0b5bd86
Andreas Tarandi <torandi@gmail.com>
2012-10-29 01:35:27 +0100

    Bah

--- view/list_entry.php
+++ view/list_entry.php
@@ -8,5 +8,6 @@
 	<h3 class="title"><a href="/download/<?=$cur->entry_id?>"><?=str_replace(' ', '&nbsp;', $cur->title)?></a></h3>
 	<span class="author">av <b><?=$cur->author?></b> (rev <?=$cur->get_revision()?>)</span>
 	<div class="description"><?=str_replace("\n", "<br/>", $cur->description)?></div>
+	<p class="clear" />
 </div>
 <?php } ?>
