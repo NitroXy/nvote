143ccddbdbb67bea5fceba434ec4b7c1ac508ab8
root <root@monster.(none)>
2012-10-30 17:14:38 +0000

    fixar räksmörgåsar

--- controller/generate.php
+++ controller/generate.php
@@ -1,5 +1,8 @@
 <?php
 
+$output_htmlspecialchars = false;
+setlocale(LC_CTYPE, "en_US.UTF-8");
+
 function create_poster($n, $entry){
 	global $e, $d;
 	$app = realpath(dirname(__FILE__) . "/../create_slide");
@@ -9,6 +12,9 @@ function create_poster($n, $entry){
 	$title = escapeshellarg($entry->title);
 	$poster = escapeshellarg("$dst-0.mkv");
 	$cmd = "$app $src $author $title $poster";
+	echo $cmd . "\n";
+	flush();
+	ob_flush();
 	exec($cmd);
 	return $poster;
 }
@@ -20,6 +26,8 @@ $final = "$e/final";
 if ( !file_exists($final) ){
 	mkdir($final);
 }
+
+header('Content-Type: text/plain');
 
 foreach ( $category as $cur ){
 	$d = "$final/{$cur->dirname()}";
