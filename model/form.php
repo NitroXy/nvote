9349fba6224c30934b6c84d1dbac1d0736f6022e
Andreas Tarandi <torandi@gmail.com>
2013-07-10 22:27:25 +0200

    ADd tworows to p layout

--- model/form.php
+++ model/form.php
@@ -499,11 +499,19 @@ class FormLayout {
 
 class FormLayoutP extends FormLayout {
 	public function add_row($label, $field, $error, $hint){
+		$hints = $field->layout_hints();
 		echo "	<p>\n";
-		if($label !== false) echo "	<span class=\"form-label\">$label</span>\n";
-		echo "	<span class=\"form-field\">{$field->get_content()}</span>\n";
-		if($error !== false) echo "	<span class=\"form-error\">$error</span>\n";
-		if($hint !== false) echo "	<span class=\"form-hint\">$hint</span>\n";
+		if ( !($hints & LAYOUT_TWOROWS) ){
+			if($label !== false) echo "	<span class=\"form-label\">$label</span>\n";
+			if($error !== false) echo "	<span class=\"form-error\">$error</span>\n";
+			if($hint !== false) echo "	<span class=\"form-hint\">$hint</span>\n";
+			echo "	<br/><span class=\"form-field\">{$field->get_content()}</span>\n";
+		} else {
+			if($label !== false) echo "	<span class=\"form-label\">$label</span>\n";
+			echo "	<span class=\"form-field\">{$field->get_content()}</span>\n";
+			if($error !== false) echo "	<span class=\"form-error\">$error</span>\n";
+			if($hint !== false) echo "	<span class=\"form-hint\">$hint</span>\n";
+		}
 		echo "	</p>\n";
 	}
 }
