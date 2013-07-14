93fcc3025d6ebf9b74cacf6dc8ef9ded8fc1692b
Andreas Tarandi <torandi@gmail.com>
2013-07-14 12:06:06 +0200

    Add function for listing all uncreated events

--- model/event.php
+++ model/event.php
@@ -8,4 +8,14 @@ class Event extends BasicObject {
 	public function info() {
 		return NXAPI::event_info(array('event' => $this->short_name));
 	}
+
+	public static function uncreated() {
+		$events = array_map(function($e) {
+			return $e->short_name;
+		}, Event::selection());
+
+		return array_filter(NXAPI::events(), function($e) use ($events) {
+			return !in_array($e->short_name, $events);
+		});
+	}
 }
