c402fd6595981f217a1302a3c78e006027804d07
Andreas Tarandi <torandi@gmail.com>
2013-10-02 18:41:01 +0200

    Add variables for category statuses

--- model/category.php
+++ model/category.php
@@ -1,8 +1,16 @@
 <?php
 
 class Category extends ValidatingBasicObject {
+	/* statuses */
+	public static $HIDDEN = 'hidden';
+	public static $VISIBLE = 'visible';
+	public static $ENTRY_OPEN = 'entry_open';
+	public static $ENTRY_CLOSED = 'entry_closed';
+	public static $VOTING_OPEN = 'voting_open';
+	public static $VOTING_CLOSE = 'voting_closed';
+	public static $RESULTS_PUBLIC = 'results_public';
 
-	public static $entries_show_statuses = array('vote_open', 'vote_closed', 'results_public');
+	public static $entries_show_statuses = array('voting_open', 'voting_closed', 'results_public');
 
 	protected function validation_hooks() {
 		$this->validate_presence_of('event_id');
@@ -19,19 +27,19 @@ class Category extends ValidatingBasicObject {
 	}
 
 	public function visible() {
-		return $this->status != 'hidden';
+		return $this->status != Category::$HIDDEN;
 	}
 
 	public function voting_open() {
-		return $this->status == 'voting_open';
+		return $this->status == Category::$VOTING_OPEN;
 	}
 
 	public function entry_open() {
-		return $this->status == 'entry_open';
+		return $this->status == Category::$ENTRY_OPEN;
 	}
 
 	public function results_public() {
-		return $this->status == 'results_public';
+		return $this->status == Category::$RESULTS_PUBLIC;
 	}
 
 }
